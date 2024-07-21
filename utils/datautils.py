13ea6051dfef622ed382150ab1c607ac297e1d79
AmirhosseinBZRG <118589755+AmirhosseinBZRG@users.noreply.github.com>
2024-07-21 13:30:51 +0330

    Update datautils.py

--- utils/datautils.py
+++ utils/datautils.py
@@ -33,6 +33,33 @@ def get_wikitext2(nsamples, seed, seqlen, model):
     return trainloader, testenc
 
 
+def get_hellaswag(nsamples, seed, seqlen, model):
+    from datasets import load_dataset
+    dataset = load_dataset('hellaswag')
+    traindata = dataset['train']
+    valdata = dataset['validation']
+
+    from transformers import AutoTokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model, use_fast=False)
+
+    # Concatenate the 'context' and 'ending' fields for each sample
+    trainenc = tokenizer([f"{context} {ending}" for context, ending in zip(traindata['context'], traindata['endings'][:, 0])], return_tensors='pt')
+    testenc = tokenizer([f"{context} {ending}" for context, ending in zip(valdata['context'], valdata['endings'][:, 0])], return_tensors='pt')
+
+
+    import random
+    random.seed(seed)
+    trainloader = []
+    for _ in range(nsamples):
+        i = random.randint(0, trainenc.input_ids.shape[1] - seqlen - 1)
+        j = i + seqlen
+        inp = trainenc.input_ids[:, i:j]
+        tar = inp.clone()
+        tar[:, :-1] = -100
+        trainloader.append((inp, tar))
+    return trainloader, testenc
+
+
 def get_ptb(nsamples, seed, seqlen, model):
     from datasets import load_dataset
     traindata = load_dataset('ptb_text_only', 'penn_treebank', split='train')
@@ -178,6 +205,8 @@ def get_c4_new(nsamples, seed, seqlen, model):
 
 def get_loaders(name, nsamples=128, seed=0, seqlen=2048, model=''):
     if 'wikitext2' in name:
+        return get_wikitext2(nsamples, seed, seqlen, model)
+    if 'hellaswag' in name:
         return get_wikitext2(nsamples, seed, seqlen, model)
     if 'ptb' in name:
         if 'new' in name:
